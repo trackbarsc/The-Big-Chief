e633eeef538fc633ac8c48345e428ce45def18c4
trackbarsc <trackbar.sc@gmail.com>
2026-03-14 15:43:13 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'allison-cc-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'bigchief-v2';
+const ASSETS = ['./index.html', './manifest.json', './logo.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,10 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
